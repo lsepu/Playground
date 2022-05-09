70beefdbd149f562e4d960eae95579e2dc3b521c
lsepu <55538804+lsepu@users.noreply.github.com>
2022-05-09 17:26:36 -0500

    Argument capture added and test exceptions

--- SpringStudents/demo/src/test/java/com/example/demo/student/StudentServiceTest.java
+++ SpringStudents/demo/src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -5,11 +5,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static java.time.Month.JANUARY;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,8 +43,44 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
     void addNewStudent() {
+        //given
+        Student student = new Student(
+                "Mariam",
+                "mariam@gmail.com",
+                LocalDate.of(2000, JANUARY, 5)
+        );
+        //when
+        underTest.addNewStudent(student);
+        //then
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+
+        Student capturedStudent =  studentArgumentCaptor.getValue();
+
+        assertThat(capturedStudent).isEqualTo(student);
+    }
+
+    @Test
+    void willThrownWhenEmailIsTaken() {
+        //given
+        Student student = new Student(
+                "Mariam",
+                "mariam@gmail.com",
+                LocalDate.of(2000, JANUARY, 5)
+        );
+
+        given(studentRepository.findStudentByEmail(anyString())).willReturn(Optional.of(student));
+        //when
+        //then
+       assertThatThrownBy(() -> underTest.addNewStudent(student))
+               .isInstanceOf(IllegalStateException.class)
+               .hasMessageContaining(
+                       "email taken");
+
+       //the method was never call, it's never going to save a student
+        verify(studentRepository, never()).save(any());
     }
 
     @Test
